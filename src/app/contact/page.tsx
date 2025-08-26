54d6bb3c88add1814342307703ff088e6ba4a0df
Lynn Pyae Hmue <orionlynn@Lynns-MacBook-Air-5.local>
2025-08-26 17:18:57 +0700

    mapEmbed added to contact-us page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import SocialIcon from "./socialicons";
+import MapEmbed from "@/components/MapEmbed";
 
 {
   /* Bottom: Social Icons */
@@ -94,6 +95,10 @@ export default function Contact() {
           </div>
         </div>
       </section>
+
+      <div className=" pl-8 pr-8 pt-8 lg:pb-16 bg-white-50">
+          <MapEmbed src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3875.5!2d100.5!3d13.7!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTPCsDQyJzAwLjAiTiAxMDDCsDMwJzAwLjAiRQ!5e0!3m2!1sen!2sth!4v1234567890" />
+        </div>
 
       {/* Bottom: Social Icons */}
       <div className="mt-3 md:mt-12 grid grid-cols-2 gap-4 sm:flex sm:justify-center sm:gap-6">
