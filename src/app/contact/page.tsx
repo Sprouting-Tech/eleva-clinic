16f4bb9f8e8f67611f8233d55cc96465d798c565
Aung Kaung Myat <aungkaungmyat.kk3@gmail.com>
2025-08-26 16:27:57 +0700

    made Contact Us first Page,done web and mobile responsive.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,35 @@
+import Image from "next/image";
+import SocialIcon from "./socialicons";
+
+{
+  /* Bottom: Social Icons */
+}
+<div className="mt-12 flex justify-center gap-6 flex-wrap">
+  <SocialIcon
+    platform="Instagram"
+    username="eleva_clinic"
+    filename="instagram.png"
+    url="https://instagram.com/eleva_clinic"
+  />
+  <SocialIcon
+    platform="TikTok"
+    username="@eleva_clinic"
+    filename="tiktok.png"
+    url="https://www.tiktok.com/@eleva_clinic"
+  />
+  <SocialIcon
+    platform="Facebook"
+    username="Eleva Clinic"
+    filename="facebook.png"
+    url="https://facebook.com/eleva_clinic"
+  />
+  <SocialIcon
+    platform="Twitter"
+    username="@eleva_clinic"
+    filename="twitter.png"
+    url="https://twitter.com/eleva_clinic"
+  />
+</div>;
 export default function Contact() {
   return (
     <main className="min-h-screen bg-gradient-to-l from-[#FFE9E5] to-[#FFFFFF] px-6 py-12 text-gray-800">
@@ -7,31 +39,88 @@ export default function Contact() {
           <img
             src="/images/contact-docs.png"
             alt="Eleva Clinic Team"
-            className="bg-transparent max-w-[90%] h-auto"
+            className="bg-transparent max-w-[70%] sm:max-w-[90%] h-auto "
           />
         </div>
 
         {/* Right: Contact Info */}
-        <div className="text-left">
-          <h1 className="text-4xl text-[#B77762] mb-4">Contact Us</h1>
-          <p className="text-lg mb-2">
-            654/2 Rama IV Road, Maha Phrueatharam Subdistrict,
-            <br />
-            Bang Rak District, Bangkok 10500
-          </p>
-          <p className="text-md mb-2">📞 098-994-1698</p>
-          <p className="text-md mb-6">
-            🕒 Open daily 11:00 - 20:00 (Except Wednesday)
-          </p>
+        <div className="text-left space-y-4 pb-6 md:pb-30">
+          <h1 className="text-3xl md:text-4xl text-[#B77762] mb-4 text-center md:text-left">
+            Contact Us
+          </h1>
+
+          {/* Address */}
+          <div className="flex items-start gap-3 mx-auto sm:mx-0">
+            <Image
+              src="/images/location1.png"
+              alt="Location Icon"
+              width={30}
+              height={30}
+              className="w-[20px] h-[25px]"
+            />
+            <p className=" text-xs md:text-lg">
+              654/2 Rama IV Road, Maha Phrueatharam
+              <br className="hidden md:inline" />
+              Subdistrict,Bang Rak District, Bangkok
+              <br className="hidden md:inline" />
+              10500
+            </p>
+          </div>
+
+          {/* Phone */}
+          <div className="flex items-center gap-3 mx-auto sm:mx-0">
+            <Image
+              src="/images/phone.png"
+              alt="Phone Icon"
+              width={30}
+              height={30}
+              className="w-[25px] h-[25px]"
+            />
+            <p className="text-xs md:text-lg">098-994-1698</p>
+          </div>
+
+          {/* Hours */}
+          <div className="flex items-center gap-3 mx-auto sm:mx-0 md:mb-2">
+            <Image
+              src="/images/clock.png"
+              alt="Clock Icon"
+              width={30}
+              height={30}
+              className="w-[25px] h-[25px]"
+            />
+            <p className="text-xs md:text-lg">
+              Open daily 11:00 - 20:00 (Except Wednesday)
+            </p>
+          </div>
         </div>
       </section>
 
       {/* Bottom: Social Icons */}
-      <div className="mt-12 flex justify-center gap-6 flex-wrap">
-        {/* <SocialIcon platform="Instagram" filename="instagram.png" url="https://instagram.com/eleva_clinic" />
-        <SocialIcon platform="Facebook" filename="facebook.png" url="https://facebook.com/eleva_clinic" />
-        <SocialIcon platform="TikTok" filename="tiktok.png" url="https://www.tiktok.com/@eleva_clinic" />
-        <SocialIcon platform="Line" filename="line.png" url="https://line.me/ti/p/~eleva_clinic" /> */}
+      <div className="mt-3 md:mt-12 grid grid-cols-2 gap-4 sm:flex sm:justify-center sm:gap-6">
+        <SocialIcon
+          platform="Instagram"
+          username="eleva_clinic"
+          filename="instagram.png"
+          url="https://www.instagram.com/eleva_clinic"
+        />
+        <SocialIcon
+          platform="TikTok"
+          username="@eleva_clinic"
+          filename="tiktok.png"
+          url="https://www.tiktok.com/@eleva_clinic"
+        />
+        <SocialIcon
+          platform="Facebook"
+          username="Eleva Clinic"
+          filename="facebook.png"
+          url="https://www.facebook.com/elevaclinic/"
+        />
+        <SocialIcon
+          platform="Twitter"
+          username="@eleva_clinic"
+          filename="twitter.png"
+          url="https://x.com/"
+        />
       </div>
     </main>
   );
